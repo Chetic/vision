1b00f6581d5d741abac8d9ed61ae57566a6851f8
Chetic <fredrik@chetech.se>
2022-08-06 21:25:46 +0200

    upgrade plugins and make vision work with CLion 2022.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,15 +9,15 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "1.4.32"
+    id("org.jetbrains.kotlin.jvm") version "1.7.10"
     // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-    id("org.jetbrains.intellij") version "0.7.3"
+    id("org.jetbrains.intellij") version "1.8.0"
     // gradle-changelog-plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
     id("org.jetbrains.changelog") version "1.1.2"
     // detekt linter - read more: https://detekt.github.io/detekt/gradle.html
-    id("io.gitlab.arturbosch.detekt") version "1.16.0"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
     // ktlint linter - read more: https://github.com/JLLeitschuh/ktlint-gradle
-    id("org.jlleitschuh.gradle.ktlint") version "10.0.0"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
 group = properties("pluginGroup")
@@ -29,20 +29,21 @@ repositories {
     jcenter()
 }
 dependencies {
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.16.0")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0")
 }
 
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
 intellij {
-    pluginName = properties("pluginName")
-    version = properties("platformVersion")
-    type = properties("platformType")
-    downloadSources = properties("platformDownloadSources").toBoolean()
-    updateSinceUntilBuild = true
+    //pluginName = properties("pluginName")
+    //version = properties("platformVersion")
+    //type = properties("platformType")
+    //downloadSources = properties("platformDownloadSources").toBoolean()
+    //updateSinceUntilBuild = true
 
-    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
-    setPlugins(*properties("platformPlugins").split(',').map(String::trim).filter(String::isNotEmpty).toTypedArray())
+    version.set("2022.2")
+    type.set("CL")
+    plugins.set(listOf("cidr-base-plugin","clion","c-plugin"))
 }
 
 // Configure gradle-changelog-plugin plugin.
@@ -78,7 +79,7 @@ tasks {
     withType<Detekt> {
         jvmTarget = "1.10"
     }
-
+/*
     patchPluginXml {
         version(properties("pluginVersion"))
         sinceBuild(properties("pluginSinceBuild"))
@@ -106,17 +107,17 @@ tasks {
             }
         )
     }
-
+*/
     runPluginVerifier {
-        ideVersions(properties("pluginVerifierIdeVersions"))
+        //ideVersions(properties("pluginVerifierIdeVersions"))
     }
 
     publishPlugin {
         dependsOn("patchChangelog")
-        token(System.getenv("PUBLISH_TOKEN"))
+        //token(System.getenv("PUBLISH_TOKEN"))
         // pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
         // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
         // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
-        channels(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first())
+        //channels(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first())
     }
 }
